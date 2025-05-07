e60692ee3739fb97971ac7191e8fe986a2507567
Victor <guzun.victor@gmail.com>
2025-05-07 18:36:33 +0300

    add rxBYtes and txBytes to Stats

--- lib/model/stats.dart
+++ lib/model/stats.dart
@@ -2,16 +2,18 @@ class Stats {
   final num totalDownload;
   final num totalUpload;
   final num lastHandshake;
+  final num rxBytes;
+  final num txBytes;
 
   /// Constructor of the [Stats] class that receives [totalDownload] where total downloaded data is stored,
   /// [totalUpload] where uploaded data is stored.
-  Stats({required this.totalDownload, required this.totalUpload, required this.lastHandshake});
+  Stats({required this.totalDownload, required this.totalUpload, required this.lastHandshake, required this.rxBytes, required this.txByte});
 
   /// Method [toJson] to convert the class to JSON.
-  Map<String, dynamic> toJson() => {'totalDownload': totalDownload, 'totalUpload': totalUpload, 'lastHanshake': lastHandshake};
+  Map<String, dynamic> toJson() => {'totalDownload': totalDownload, 'totalUpload': totalUpload, 'lastHanshake': lastHandshake , 'rxBytes': rxBytes, 'txBytes': txBytes};
 
   /// Method [Stats.fromJson] to convert the JSON to class.
   factory Stats.fromJson(Map<String, dynamic> json) {
-    return Stats(totalDownload: json['totalDownload'] as num, totalUpload: json['totalUpload'] as num, lastHandshake: json['lastHandshake'] as num);
+    return Stats(totalDownload: json['totalDownload'] as num, totalUpload: json['totalUpload'] as num, lastHandshake: json['lastHandshake'] as num, rxBytes: json['rxBytes'] as num, txBytes: json['txBytes'] as num);
   }
 }
